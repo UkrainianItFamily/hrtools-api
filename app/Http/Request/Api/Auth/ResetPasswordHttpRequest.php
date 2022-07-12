898f9de50fc56c6a8c1b67ebea2a170397ed1ce1
vadym980 <vadim.gmyrya@ukrainian-it.family>
2022-07-12 22:58:38 +0300

    Feat (Request) added new requests

---
+++ app/Http/Request/Api/Auth/ResetPasswordHttpRequest.php
@@ -0,0 +1,22 @@
+<?php
+
+declare(strict_types=1);
+
+namespace App\Http\Request\Api\Auth;
+
+use App\Http\Request\ApiFormRequest;
+
+final class ResetPasswordHttpRequest extends ApiFormRequest
+{
+    /**
+     * Get the validation rules that apply to the request.
+     *
+     * @return array
+     */
+    public function rules(): array
+    {
+        return [
+            'email' => 'required|email',
+        ];
+    }
+}
