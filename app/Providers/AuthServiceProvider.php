7d99ee5e80b5d853304abd841bf427e458564191
vadym980 <108681911+vadym980@users.noreply.github.com>
2022-07-19 19:59:43 +0300

    feat edited url for mail of password reset

--- app/Providers/AuthServiceProvider.php
+++ app/Providers/AuthServiceProvider.php
@@ -2,8 +2,8 @@
 
 namespace App\Providers;
 
+use Illuminate\Auth\Notifications\ResetPassword;
 use Illuminate\Foundation\Support\Providers\AuthServiceProvider as ServiceProvider;
-use Illuminate\Support\Facades\Gate;
 
 class AuthServiceProvider extends ServiceProvider
 {
@@ -25,6 +25,8 @@ class AuthServiceProvider extends ServiceProvider
     {
         $this->registerPolicies();
 
-        //
+        ResetPassword::createUrlUsing(function ($user, string $token) {
+            return 'https://hr-tools-academy-front.herokuapp.com/auth/reset-passпше ?token='.$token.'&email=' . $user->email;
+        });
     }
 }
