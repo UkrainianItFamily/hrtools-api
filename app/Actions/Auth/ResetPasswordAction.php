15fb85aa95f1041953ae829b5f240d34f1e3fecd
vadym980 <108681911+vadym980@users.noreply.github.com>
2022-07-19 19:35:14 +0300

    fix (action) improved file

--- app/Actions/Auth/ResetPasswordAction.php
+++ app/Actions/Auth/ResetPasswordAction.php
@@ -7,50 +7,18 @@ namespace App\Actions\Auth;
 use App\Http\Controllers\Api\ApiController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Password;
-use Illuminate\Contracts\Auth\PasswordBroker;
 
 final class ResetPasswordAction extends ApiController
 {
     public function execute(Request $request)
     {
-        $this->validateEmail($request);
-
-        $response = $this->broker()->sendResetLink(
-            $this->credentials($request)
+        $response = Password::sendResetLink(
+            $request->only('email')
         );
 
-        return $response == Password::RESET_LINK_SENT
-            ? $this->sendResetLinkResponse($response)
-            : $this->sendResetLinkFailedResponse();
-    }
+        return $response === Password::RESET_LINK_SENT
+            ? ['status' => __($response)]
+            : ['email' => __($response)];
 
-    private function validateEmail(Request $request): void
-    {
-        $request->validate(['email' => 'required|email']);
-    }
-
-    private function credentials(Request $request): array
-    {
-        return $request->only('email');
-    }
-
-    private function sendResetLinkResponse($response): array
-    {
-        return [
-            'message' => 'Mail has been sent for password reset',
-            'data' => $response,
-        ];
-    }
-
-    private function sendResetLinkFailedResponse(): array
-    {
-        return [
-            'message' => 'Mail has not been sent, try again later'
-        ];
-    }
-
-    private function broker(): PasswordBroker
-    {
-        return Password::broker();
     }
 }
