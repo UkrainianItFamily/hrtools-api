753a5dabbe0edd3d1f566143c7235d89c784ac52
AndriiOleinych <work.oleinitch@gmail.com>
2022-07-25 14:35:50 +0300

    add routes

--- routes/api.php
+++ routes/api.php
@@ -1,8 +1,11 @@
 <?php
 
-use App\Http\Controllers\Api\StatusController;
+use App\Http\Controllers\Api\RegistrationController;
+use App\Http\Controllers\Api\VerificationController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\Api\StatusController;
+use App\Http\Controllers\Api\AuthController;
 
 /*
 |--------------------------------------------------------------------------
@@ -22,3 +25,19 @@ Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
 Route::get('/status/{serviceName?}', [StatusController::class, 'status']);
 Route::post('/mail', [StatusController::class, 'mail']);
 Route::post('/broadcast', [StatusController::class, 'event']);
+
+Route::prefix('v1')->group(function () {
+    Route::prefix('auth')->group(function () {
+        Route::post('login', [AuthController::class, 'login'])->name('login');
+        Route::post('register', [RegistrationController::class, 'register']);
+        Route::post('forgot-password', [AuthController::class, 'forgotPassword'])->name('forgot-password');
+        Route::post('reset', [AuthController::class, 'reset'])->name('reset-password');
+        Route::post('logout', [AuthController::class, 'logout'])->name('logout');
+    });
+
+    Route::prefix('email')->group(function (){
+        Route::post('verify/{id}', [VerificationController::class, 'verify'])->name('verification.verify');
+        Route::post('resend/{id}', [VerificationController::class, 'resend'])->name('verification.resend');
+    });
+
+});
